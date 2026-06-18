6507cb45a5f9c6f6c3b504a622618d5d5d8f44e5
Mustafa Zeb <mustafazeb381@gmail.com>
2026-06-18 17:20:20 +0300

    added the gaurd to api

---
+++ src/guards/auth/auth.guard.ts
@@ -0,0 +1,14 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const authHeader = request.headers['autherization'];
+
+    return authHeader === '1234567890';
+  }
+}
